7c0e0b9c61a54124e9ac647d8c104a1f268c3e7f
thainhatminh2005 <minh14012001@gmail.com>
2024-03-18 14:37:13 +0700

    First commit!

---
+++ projectObj.h
@@ -0,0 +1,25 @@
+#ifndef __PROJECTOBJ__
+#define __PROJECTOBJ__
+
+struct Int256
+{
+    char value[32];
+    char hex[64];
+
+    Int256(char [], int);
+
+    char* conv2hex();
+    char* conv2char();
+
+    void assign(Int256*);
+    void index(int);
+};
+
+Int256 pls(Int256*, Int256*, Int256*);
+Int256 sub(Int256*, Int256*, Int256*);
+Int256 mul(Int256*, Int256*, Int256*);
+Int256 pow(Int256*, Int256*, Int256*);
+Int256 mod(Int256*, Int256*);
+Int256 div(Int256*, Int256*);
+
+#endif
